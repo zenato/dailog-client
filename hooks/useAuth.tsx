a9b4fb8a22fcc9cff5adcdc65cd0b4a620e569a3
Yeongjin Lee <yeongjinnn@gmail.com>
2020-01-31 14:29:01 +0900

    Remove dependency of useCallback

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -7,7 +7,7 @@ export default function useAuth() {
   const user = useSelector((state: RootState) => state.common.user)
   const dispatch = useDispatch()
 
-  const onSetUser = useCallback((user: User) => dispatch(setUser(user)), [dispatch, user])
+  const onSetUser = useCallback((user: User) => dispatch(setUser(user)), [dispatch])
   const onLogout = useCallback(() => dispatch(logout()), [dispatch])
 
   return {
