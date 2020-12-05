eab7e8ee12fe1dc75fa6bc242a91045bdb9a467d
Yeongjin Lee <yeongjinnn@gmail.com>
2020-12-05 20:07:27 +0900

    Apply SEO

--- components/common/Head/Head.tsx
+++ components/common/Head/Head.tsx
@@ -1,12 +1,17 @@
 import { FC } from 'react'
 import NextHead from 'next/head'
+import { DefaultSeo } from 'next-seo'
+import config from '@config/seo.json'
 
 const Head: FC = () => {
   return (
-    <NextHead>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <title>Dailog</title>
-    </NextHead>
+    <>
+      <DefaultSeo {...config} />
+      <NextHead>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Dailog</title>
+      </NextHead>
+    </>
   )
 }
 
